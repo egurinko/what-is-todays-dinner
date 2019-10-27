4bab2eb486f5904fce1cbbbfa527d6f994db581b
egurinko <egctoru@gmail.com>
2019-10-27 21:51:32 +0900

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <button
+    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+  >
+    Button
+  </button>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 
 export default Vue.extend({
-  name: "app",
-  components: {
-    HelloWorld
-  }
+  name: "app"
 });
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueCompositionApi from '@vue/composition-api';
 import App from "./App.vue";
 import router from "./router";
+import '@/assets/css/tailwind.css';
 
 Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
