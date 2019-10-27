ee1d1031bd640515959eeec264745ec79185b38e
egurinko <egctoru@gmail.com>
2019-10-27 23:23:39 +0900

    delete route

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import Vue from "vue";
 import VueCompositionApi from '@vue/composition-api';
 import App from "./App.vue";
-import router from "./router";
 import '@/assets/css/tailwind.css';
 
 Vue.use(VueCompositionApi);
 Vue.config.productionTip = false;
 
+declare module '@vue/composition-api/dist/component/component' {
+  interface SetupContext {
+    readonly refs: { [key: string]: Vue | Element | Vue[] | Element[] };
+  }
+}
+
 new Vue({
-  router,
   render: h => h(App)
 }).$mount("#app");
