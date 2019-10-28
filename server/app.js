211a92bdb386a65f5e912b76081de8b5dd7a604c
egurinko <egctoru@gmail.com>
2019-10-29 00:21:06 +0900

    added api router to server

--- server/app.js
+++ server/app.js
@@ -1,40 +1,57 @@
 // BASIC EXPRESS SETUPs
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-// const apiRouter = require('./api');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const apiRouter = require("./routes")();
 
 const app = express();
 
 // LOGGING SETTINGs
 app.use(
   morgan(
-    ":remote-addr - :remote-user [:date[clf]] ':method :url HTTP/:http-version' :status :res[content-length] :response-time ms",
-  ),
+    ":remote-addr - :remote-user [:date[clf]] ':method :url HTTP/:http-version' :status :res[content-length] :response-time ms"
+  )
 );
 
 // URL Encode for POST requests
 app.use(
   bodyParser.urlencoded({
-    extended: true,
-  }),
+    extended: true
+  })
 );
 
 // Extract JSON data from POST requests
 app.use(bodyParser.json());
 
-// SERVE STATIC FILEs
-app.use(express.static(path.join(__dirname, '/../dist/')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/../dist/index.html'));
-});
+// CROSS DOMAIN
+const allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, x-access-token"
+  );
+
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 
 // SEND TO APIs
-// app.use(
-//   '/api', // add additional middleware to the server, mounted on the /api/ path
-//   apiRouter,
-// );
+app.use(
+  "/api", // add additional middleware to the server, mounted on the /api/ path
+  apiRouter
+);
+
+// SERVE STATIC FILEs
+app.use(express.static(path.join(__dirname, "/../dist/")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/../dist/index.html"));
+});
 
 module.exports = app;
