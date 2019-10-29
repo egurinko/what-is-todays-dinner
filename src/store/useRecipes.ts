a040622903fe2f61e835d597deee34a00aae8e4b
egurinko <egctoru@gmail.com>
2019-10-30 01:20:21 +0900

    added recipes state

---
+++ src/store/useRecipes.ts
@@ -0,0 +1,40 @@
+import Vue from "vue";
+import VueCompositionApi from '@vue/composition-api';
+Vue.use(VueCompositionApi);
+
+import { ref } from "@vue/composition-api";
+
+export type Recipe = {
+  foodImageUrl: string;
+  recipeDescription: string;
+  recipePublishday: string;
+  shop: number;
+  pickup: number;
+  recipeId: number;
+  nickname: string;
+  smallImageUrl: string;
+  recipeMaterial: string[];
+  recipeIndication: string;
+  recipeCost: string;
+  rank: number;
+  recipeUrl: string;
+  mediumImageUrl: string;
+  recipeTitle: string;
+}
+
+export type Recipes = Recipe[]
+
+const recipes = ref<Recipes>([]);
+
+const useRecipes = () => {
+  const getRecipes = (newRecipes: Recipes) => {
+    recipes.value = newRecipes;
+  }
+
+  return {
+    recipes,
+    getRecipes
+  };
+};
+
+export default useRecipes;
