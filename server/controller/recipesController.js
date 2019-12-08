2b39a4aa9f79f0d06edffb51e7ee8c1f865e1419
egurinko <egctoru@gmail.com>
2019-12-08 18:58:38 +0900

    change backend to allow to use query parameter

--- server/controller/recipesController.js
+++ server/controller/recipesController.js
@@ -1,8 +1,9 @@
 const getRecipes = require("../services/getRecipes");
 
 exports.list = async (req, res) => {
+  const searchText = req.query.searchText;
   try {
-    const data = await getRecipes();
+    const data = await getRecipes(searchText);
     res.status(200).send(data);
   } catch (err) {
     res.status(500).send(err);
--- server/services/getRecipes.js
+++ server/services/getRecipes.js
@@ -1,24 +1,27 @@
-const axios = require("axios");
 require("dotenv").config();
-const RAKETEN_APPLICATION_ID = process.env.RAKETEN_APPLICATION_ID;
-
 const config = require("../../config");
 const MongoClient = require("mongodb").MongoClient;
 const parser = { useNewUrlParser: true, useUnifiedTopology: true };
 
-module.exports = async () => {
+module.exports = async searchText => {
   const client = MongoClient(config.db.mongoURL, parser);
   try {
     await client.connect();
     const db = client.db(config.db.dbName);
 
-    const data = await db
+    let data = await db
       .collection("recipes")
       .find()
       .toArray();
 
+    if (searchText !== "") {
+      data = data.filter(recipe => {
+        return recipe.recipeMaterial.includes(searchText);
+      });
+    }
+
     client.close();
-    return data;
+    return spliced;
   } catch (err) {
     console.log(err);
   }
